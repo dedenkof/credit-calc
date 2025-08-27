975f9d820ba2439e9083f864407ba78d913d6bc5
Volodymyr Dedenko <dedenkof@ukr.net>
2025-08-27 14:23:24 +0300

    Add user form component and create model for input date

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+// import { Component, output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,18 +11,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+	// calculate = output();
+	@Output() calculate = new EventEmitter<InvestmentInput>();
+
 	enteredInitialInvestment = '0';
-	enteredAnnualInvestment = '0';
-	enteredExpectedReturn = '5';
 	enteredDuration = '10';
+	enteredExpectedReturn = '5';
+	enteredAnnualInvestment = '0';
+	
 	
 	onSubmit() {
-		console.log('Submited');
-
-	console.log(this.enteredInitialInvestment);
-	console.log(this.enteredAnnualInvestment);
-	console.log(this.enteredExpectedReturn);
-	console.log(this.enteredDuration);
-
+		this.calculate.emit({
+			initialInvestment: +this.enteredInitialInvestment,
+			duration: +this.enteredDuration,
+			expectedReturn: +this.enteredExpectedReturn,
+			annualInvestment: +this.enteredAnnualInvestment
+	});
 	}
 }
