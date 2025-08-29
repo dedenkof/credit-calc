b1a9d68c0a53cc98f8f9c0457be8e3d21d38fca4
Volodymyr Dedenko <dedenkof@ukr.net>
2025-08-29 17:13:05 +0300

    Change project to service approach

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
-import type { InvestmentInput } from './investment-input.model';
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
@@ -12,47 +11,5 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-	resultsData = signal<{
-      year: number,
-      interest: number,
-      valueEndOfYear: number,
-      annualInvestment: number,
-      totalInterest: number,
-      totalAmountInvested: number,
-    }[] | undefined>(undefined);
-
-	// ? : {
-  //     year: number,
-  //     interest: number,
-  //     valueEndOfYear: number,
-  //     annualInvestment: number,
-  //     totalInterest: number,
-  //     totalAmountInvested: number,
-  //   }[]
-
-	onCalculateInvestmentResults(data: InvestmentInput) {
-	
-	const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-  const annualData = [];
-  let investmentValue = initialInvestment;
-
-  for (let i = 0; i < duration; i++) {
-    const year = i + 1;
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    const totalInterest =
-      investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
-      year: year,
-      interest: interestEarnedInYear,
-      valueEndOfYear: investmentValue,
-      annualInvestment: annualInvestment,
-      totalInterest: totalInterest,
-      totalAmountInvested: initialInvestment + annualInvestment * year,
-    });
-  }
-
-  this.resultsData.set(annualData);
-}
 
 }
